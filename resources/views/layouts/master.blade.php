4ab199a361da96524f03d3b7c38d72bba55e13b6
maehner <maehner@kontrollfeld.de>
2018-03-02 11:41:48 +0100

    save

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -26,13 +26,11 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Project name</a>
+          <a class="navbar-brand" href="#">Some Blog</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
             <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
 						@if( auth()->check() )
 						<li><a href="{{ route('dashboard') }}">Dashboard</a></li>
 						@endif
@@ -56,7 +54,7 @@
       <hr>
 
       <footer>
-        <p>&copy; 2016 Company, Inc.</p>
+        <p>&copy; 2018 Marcus Mähner</p>
       </footer>
 
     </div><!--/.container-->
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,10 +1,6 @@
 @extends ('layouts.master')
 
 @section ('content')
-	<div class="jumbotron">
-		<h1>Hello, world!</h1>
-		<p>This is an example to show the potential of an offcanvas layout pattern in Bootstrap. Try some responsive-range viewport sizes to see it in action.</p>
-	</div>
 
 	@if($posts)
 
